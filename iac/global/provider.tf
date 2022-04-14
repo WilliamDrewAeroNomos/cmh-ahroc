94fe910f0500c19ad0df827921692c9c05caf471
William Drew <wdrew@governmentcio.com>
2022-04-14 15:45:31 -0400

    Added symlinks

--- iac/global/provider.tf
+++ iac/global/provider.tf
@@ -1,1 +1,13 @@
-../provider.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.74.3"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
